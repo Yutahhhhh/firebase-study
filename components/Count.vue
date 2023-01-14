0d64b65b58808645a09dd8cd557b568fcb95fa85
yutahhh <y_hori@galileoscope.co.jp>
2023-01-15 01:29:46 +0900

    feature/#2

---
+++ components/Count.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn @click="increments">
+    <span>{{ name }}: {{ counter }}回</span>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Emit, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Count extends Vue {
+  @Prop({ type: String }) name!: string;
+  @Prop({ type: Number, default: 0 }) counter!: number;
+
+  @Emit()
+  increments(): number {
+    return this.counter;
+  }
+}
+</script>
