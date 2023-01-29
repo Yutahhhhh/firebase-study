c85a1529969b0046f2d5f2e15b9e1b02a8c8501b
yutahhh <y_hori@galileoscope.co.jp>
2023-01-29 20:04:05 +0900

    feature/#4

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
     <v-container>
-      <v-card max-width="600" class="ma-auto">
-        <v-card-title>渾身の親父ギャグ</v-card-title>
-        <v-card-text>{{ jokeTxt }}</v-card-text>
+      <v-card :style="colorSwitch === 'cool' ? coolColor : cuteColor" class="color-pallet">
+        <v-card-text v-html="richText"/>
         <v-card-actions>
-          <v-spacer/>
-          <joke-trigger @run="joke"></joke-trigger>
+          <v-btn color="#08ffc8" @click="colorSwitch='cool'">クール系</v-btn>
+          <v-btn color="#ffb6b9" @click="colorSwitch='cute'">かわいい系</v-btn>
         </v-card-actions>
       </v-card>
     </v-container>
@@ -15,16 +14,41 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import JokeTrigger from '~/components/JokeTrigger.vue';
 
-@Component({
-  components: { JokeTrigger }
-})
+type ColorType = {
+  "--color-h1": string
+  "--color-h2": string
+  "--color-h3": string
+  "--color-back": string
+}
+
+@Component
 export default class Index extends Vue {
-  jokeTxt: string = ''
-
-  joke(str: string): void {
-    this.jokeTxt = str
+  colorSwitch: 'cool' | 'cute' = 'cool'
+  coolColor: ColorType = {
+    "--color-h1": '#08ffc8',
+    "--color-h2": '#fff7f7',
+    "--color-h3": '#dadada',
+    "--color-back": '#204969'
   }
+  cuteColor: ColorType = {
+    "--color-h1": '#ffb6b9',
+    "--color-h2": '#fae3d9',
+    "--color-h3": '#bbded6',
+    "--color-back": '#8ac6d1'
+  }
+  richText: string = `<h1>h1タグだよ</h1><p>こんにちは、h1タグです。</p><h2>h2タグだよ</h2><p>こんにちは、h2タグです。</p><h3>h3タグだよ</h3><p>こんにちは、h3タグです。</p>`
 }
 </script>
+
+<style lang="sass">
+.color-pallet
+  h1
+    background: var(--color-h1)
+  h2
+    background: var(--color-h2)
+  h3
+    background: var(--color-h3)
+  p
+    color: var(--color-back)
+</style>
